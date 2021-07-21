4ccb5f6c82533c22e415a2319933f8cff2b4e4de
dongtech <zld126126@126.com>
2021-07-21 11:47:28 +0800

    package project

---
+++ MyKoa/README.md
@@ -0,0 +1,14 @@
+# myKoa
+myKoa
+
+启动
+start.bat 
+或者
+npm start
+
+访问localhost:3000/hello
+返回结果
+{
+  "name": "dongtech",
+  "title": "hello,world!"
+}
---
+++ MyKoa/routes/index.js
@@ -0,0 +1,26 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+router.get('/hello', async (ctx, next) => {
+  ctx.body = {
+    name: 'dongtech',
+    title:'hello,world!'
+  }
+})
+
+module.exports = router
